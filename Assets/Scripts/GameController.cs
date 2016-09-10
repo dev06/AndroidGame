7bf9fc0ba5a216de37504a17429cee2a7022e7d3
dev06 <devanpatel13@gmail.com>
2016-09-09 22:19:34 -0400

    change

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -18,6 +18,7 @@ public class GameController : MonoBehaviour {
 	{
 		_wall = Resources.Load("Prefabs/Wall") as GameObject;
 		walls = new List<GameObject>();
+
 		//GenerateMap();
 		GenerateWallMap();
 	}
@@ -27,16 +28,18 @@ public class GameController : MonoBehaviour {
 	int index = 1;
 	void Update ()
 	{
-		Camera.main.transform.position += new Vector3(0, .005f, 0);
+		GameObject.Find("WallObjects").transform.position = GameObject.Find("Player").transform.position;
 		counter += Time.deltaTime;
 		if (counter > .05f)
 		{
 
 
-			Destroy();
+
 			index++;
 			counter = 0;
 		}
+
+		Destroy();
 
 		if (Input.GetMouseButtonDown(0))
 		{
@@ -59,56 +62,15 @@ public class GameController : MonoBehaviour {
 			float _difference = -(_pointerDown.x - _pointerUp.x);
 			if (_difference < 0)
 			{
-				Debug.Log("Swipe Left");
+
 			} else if (_difference > 0)
 			{
-				Debug.Log("Swipe Right");
+
 			}
 		}
 
 		_pointerDown = Vector2.zero;
 		_pointerUp = Vector2.zero;
-	}
-
-	float height;
-	private void GenerateMap()
-	{
-		for (int i = 0; i < index	; i++)
-		{
-			height = Random.Range(.01f, .02f);
-			if (i > 0)
-			{
-				Transform _previousWall = walls[i - 1].transform;
-				GameObject _currentWall = Instantiate(_wall, new Vector2( 0,  0), Quaternion.identity) as GameObject;
-				_currentWall.name = "Wall" + i;
-				_currentWall.tag = "Walls";
-				Vector2 pos = Vector2.zero;
-				if (Random.Range(0, 2) == 0)
-				{
-					float rotation =  (Random.Range(0, 2) == 0) ? -90 : 90;
-					_currentWall.transform.Rotate(new Vector3(0, 0, rotation));
-					pos = new Vector2((rotation > 0) ? (_previousWall.transform.localScale.x / 2.0f * .32f) : -(_previousWall.transform.localScale.x / 2.0f * .32f) , _previousWall.transform.position.y + (_previousWall.transform.localScale.y * .32f) + (_previousWall.transform.localScale.x / 2.0f) * .32f);
-				} else
-				{
-					pos = new Vector2(0, _previousWall.transform.position.y + (_previousWall.transform.localScale.y * .32f));
-				}
-
-				_currentWall.transform.position = pos;
-				_currentWall.transform.localScale = new Vector2(.2f, height);
-				walls.Add(_currentWall);
-
-			} else {
-
-				GameObject _currentWall = Instantiate(_wall, new Vector2(0, 0), Quaternion.identity) as GameObject;
-				_currentWall.transform.localScale = new Vector2(.2f, height );
-				_currentWall.tag = "Walls";
-				walls.Add(_currentWall);
-
-			}
-
-
-			//GameObject _currentWall = Instantiate(_wall, new Vector2(0, i * .32f), Quaternion.identity) as GameObject;
-		}
 	}
 
 	public float _rotationFreq;
@@ -118,7 +80,8 @@ public class GameController : MonoBehaviour {
 
 	private void GenerateWallMap()
 	{
-		for (int i = 0; i < index; i++)
+
+		for (int i = 0; i < 5; i++)
 		{
 			GameObject _currentWall = Instantiate(_wall, new Vector2(0, -.5f), Quaternion.identity) as GameObject;
 			_currentWall.transform.parent = GameObject.Find("WallObjects").transform;
@@ -180,25 +143,25 @@ public class GameController : MonoBehaviour {
 	private void Destroy()
 	{
 
-		// if (Input.GetMouseButtonDown(1))
-		// {
-		// 	if (walls.Count > 0)
-		// 	{
-		// 		GameObject[] _walls = GameObject.FindGameObjectsWithTag("Walls");
-		// 		for (int i = 0; i < _walls.Length; i++)
-		// 		{
-		// 			Destroy(_walls[i]);
-		// 		}
+		if (Input.GetMouseButtonDown(1))
+		{
+			if (walls.Count > 0)
+			{
+				GameObject[] _walls = GameObject.FindGameObjectsWithTag("Walls");
+				for (int i = 0; i < _walls.Length; i++)
+				{
+					Destroy(_walls[i]);
+				}
 
-		// 		walls.Clear();
-
-
-		// 	}
-
-		// 	GenerateWallMap();
+				walls.Clear();
 
 
-		// }
+			}
+
+			GenerateWallMap();
+
+
+		}
 
 
 		if (Input.GetMouseButtonDown(0))
@@ -206,19 +169,19 @@ public class GameController : MonoBehaviour {
 			GameObject.Find("WallObjects").transform.rotation *= Quaternion.Euler(new Vector3(0, 0, 90));
 		}
 
-		if (walls.Count > 0)
-		{
-			GameObject[] _walls = GameObject.FindGameObjectsWithTag("Walls");
-			for (int i = 0; i < _walls.Length; i++)
-			{
-				Destroy(_walls[i]);
-			}
+		// if (walls.Count > 0)
+		// {
+		// 	GameObject[] _walls = GameObject.FindGameObjectsWithTag("Walls");
+		// 	for (int i = 0; i < _walls.Length; i++)
+		// 	{
+		// 		Destroy(_walls[i]);
+		// 	}
 
-			walls.Clear();
+		// 	walls.Clear();
 
-			GenerateWallMap();
+		// 	GenerateWallMap();
 
-		}
+		// }
 
 
 	}
