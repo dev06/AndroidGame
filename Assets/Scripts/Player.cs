5e8a47ac37cdd9b880f82c7cc00f5201a0cd7237
dev06 <devanpatel13@gmail.com>
2016-09-10 20:22:52 -0400

    Collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,14 @@ public class Player : MonoBehaviour {
 	// Use this for initialization
 
 	SpriteRenderer _sprite;
-	GameObject _currentWall;
+	GameObject _enterObject, _exitObject;
+
 	void Start () {
 		_sprite = GetComponent<SpriteRenderer>();
-		_controller = GameObject.Find("GameController").GetComponent<GameController>();
+
 	}
 
-	float _timer;
-	float _outOfboundsTimer;
-	bool _entered;
-	bool _outSide;
 
-	GameController _controller;
-	GameObject _enterObject, _exitObject;
 
 	void Update ()
 	{
@@ -38,26 +33,17 @@ public class Player : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col)
 	{
 
-
-
-
-
 	}
 
 
 	void OnTriggerExit2D(Collider2D col)
 	{
-
 		_exitObject = col.gameObject;
-
-
-
 	}
 
 	void OnTriggerStay2D(Collider2D col)
 	{
 		_enterObject = col.gameObject;
-		_currentWall = col.gameObject;
 		_sprite.color = new Color(0, 1, 1, 1);
 
 	}
